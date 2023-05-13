dc7f9c1985748c1c1b89ccf23d4b15363d77b59a
Abdouni Zakariae <abdouni.zakariae@gmail.com>
2023-05-13 19:00:23 +0100

    init

--- prisma/migrations/20230513175928_b_dprojet/migration.sql
+++ prisma/migrations/20230513175928_b_dprojet/migration.sql
@@ -7,6 +7,7 @@ CREATE TABLE `Utilisateur` (
     `updatedAt` DATETIME(3) NOT NULL,
     `published` BOOLEAN NOT NULL DEFAULT false,
 
+    UNIQUE INDEX `Utilisateur_email_key`(`email`),
     PRIMARY KEY (`Id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -18,6 +19,7 @@ CREATE TABLE `Article` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     `published` BOOLEAN NOT NULL DEFAULT false,
+    `AUTHOR` INTEGER NOT NULL,
 
     PRIMARY KEY (`Id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -35,6 +37,28 @@ CREATE TABLE `Commentaire` (
     `Id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `contenu` VARCHAR(191) NOT NULL,
+    `articleId` INTEGER NOT NULL,
 
     PRIMARY KEY (`Id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_CategorieArticle` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_CategorieArticle_AB_unique`(`A`, `B`),
+    INDEX `_CategorieArticle_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Article` ADD CONSTRAINT `Article_AUTHOR_fkey` FOREIGN KEY (`AUTHOR`) REFERENCES `Utilisateur`(`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Commentaire` ADD CONSTRAINT `Commentaire_articleId_fkey` FOREIGN KEY (`articleId`) REFERENCES `Article`(`Id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_CategorieArticle` ADD CONSTRAINT `_CategorieArticle_A_fkey` FOREIGN KEY (`A`) REFERENCES `Article`(`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_CategorieArticle` ADD CONSTRAINT `_CategorieArticle_B_fkey` FOREIGN KEY (`B`) REFERENCES `Categorie`(`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
